13aea4f0e6eafe6a81c392da7ce282ad8acfbcf1
Shubh1817 <shobu.shubham@gmail.com>
2024-09-17 06:52:21 +0530

    add : tailwind css support

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import CanvasDraw from "./components/CanvasDraw";
 
 function App() {
   return (
-    <>
+    <div className="p-24 ">
+      <p className="text-neutral-900 text-9xl">Hello World!</p>
       <CanvasDraw />
-    </>
+    </div>
   );
 }
 
